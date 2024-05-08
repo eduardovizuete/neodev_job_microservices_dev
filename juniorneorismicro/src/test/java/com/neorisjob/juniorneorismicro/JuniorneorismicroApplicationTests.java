9ece20c5e825ccc067e253ccec35cab20951d77d
edu <vizuete.eduardo@gmail.com>
2024-05-07 21:56:25 -0500

    Junior Baseline project

---
+++ juniorneorismicro/src/test/java/com/neorisjob/juniorneorismicro/JuniorneorismicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neorisjob.juniorneorismicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorneorismicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
